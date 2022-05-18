1c81bb3dc397a64c5c93a1c51d95b5effcae2398
ierfeioq <102769650+ierfeioq@users.noreply.github.com>
2022-05-18 13:36:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,28 @@ import json
 
 bot = telebot.TeleBot('5380148604:AAH5Qi5lgzQj8yufhGfS0oYxKpVANhOlJew')
 
-@bot.message_handler(commands=['start'])
-def greet(message):
-	bot.reply_to(message, f'Welcome, Please enter a valid IP address to get the detail')
 
+TEXT = """**Hola {},
+Con este bot podras geolocalizar cualquier ip**
+Solo escribe la ip y listo"""
+
+BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("Soporte ⭕️", url = "https://t.me/lord_zeper")
+        ],
+        
+    ]
+)
+
+@Bot.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=TEXT.format(update.from_user.mention),
+        reply_markup=BUTTONS,
+        disable_web_page_preview=True,
+        quote=True
+    )
 
 @bot.message_handler(func=lambda message: True)
 def IP(message):
