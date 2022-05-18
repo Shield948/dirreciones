71ad9a374a9cfdf7c959e1aa01925337eb3e6927
ierfeioq <102769650+ierfeioq@users.noreply.github.com>
2022-05-18 13:17:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-from sys import argv
-from requests import get
-
 import telebot
+import requests
+import json
 
-try:
-    token = argv[1]
-except IndexError:
-    print('Usage: python main.py 5396450834:AAEoOmv6JiLpeBaBzbi8pVz8dszITYnhTSU')
-    exit(1)
-bot_token=os.environ.get("BOT_TOKEN"),
+bot = telebot.TeleBot('5396450834:AAEoOmv6JiLpeBaBzbi8pVz8dszITYnhTSU')
 
 @bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.from_user.id, 'Send /ip <IP address> and get its location')
+def greet(message):
+	bot.reply_to(message, f'Welcome, Please enter a valid IP address to get the detail')
 
-@bot.message_handler(commands=['ip'])
-def ip(message):
-    ip = message.text
-    ip = ip[4:]
-    json = get('http://ipwhois.app/json/' + ip).json()
-    print(json)
-    if json['success'] == True and not ip == '127.0.0.1':
-        bot.send_message(message.from_user.id, 'IP: ' + json['ip'] + '\nLocation: ' + json['city'] + ', ' + json['region'] + ', ' + json['country'] + '\nTimezone: ' + json['timezone_name'])
-    else:
-        bot.send_message(message.from_user.id, 'Error')
-        
-        
 
-bot.infinity_polling()
+@bot.message_handler(func=lambda message: True)
+def IP(message):
+	url = f'https://api.freegeoip.app/json/{message.text}?apikey=a9c7ad60-4b8e-11ec-bf1d-e384c4f2ace1'
+	result = requests.get(url).content
+	parse_json = json.loads(result)
+	bot.reply_to(message, f'----------------------------------------------- \n                    IP Detail\n----------------------------------------------- \nIP: {parse_json["ip"]} \nCountry Code: {parse_json["country_code"]} \nCountry Name: {parse_json["country_name"]} \nRegion Code: {parse_json["region_code"]} \nRegion Name: {parse_json["region_name"]} \nCity: {parse_json["city"]} \nZip Code: {parse_json["zip_code"]} \nTime Zone: {parse_json["time_zone"]} \nLatitude: {parse_json["latitude"]} \nLongitude: {parse_json["longitude"]} \nMetro Code: {parse_json["metro_code"]}')
+
+bot.polling()
