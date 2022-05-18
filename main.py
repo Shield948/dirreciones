753fd864bba0a14c4ece8fd97ab129c533a9d7ad
ierfeioq <102769650+ierfeioq@users.noreply.github.com>
2022-05-18 12:36:28 -0500

    Update and rename bot.py to main.py

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import telebot
 Bot = Client(
     "Password Generator Bot",
     bot_token=os.environ.get("BOT_TOKEN"),
-    
-)    
+    api_id=int(os.environ.get("API_ID")),
+    api_hash=os.environ.get("API_HASH")
+) 
 
 try:
     token = argv[1]
