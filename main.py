d51526bb190581e64925100c4443c151b6eadbc7
ierfeioq <102769650+ierfeioq@users.noreply.github.com>
2022-05-19 09:53:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ def IP(message):
 	url = f'https://api.freegeoip.app/json/{message.text}?apikey=a9c7ad60-4b8e-11ec-bf1d-e384c4f2ace1'
 	result = requests.get(url).content
 	parse_json = json.loads(result)
-	bot.reply_to(message, f'----------------------------------------------- \n                    IP Detail\n----------------------------------------------- \nIP: {parse_json["ip"]} \nCountry Code: {parse_json["country_code"]} \nCountry Name: {parse_json["country_name"]} \nRegion Code: {parse_json["region_code"]} \nRegion Name: {parse_json["region_name"]} \nCity: {parse_json["city"]} \nZip Code: {parse_json["zip_code"]} \nTime Zone: {parse_json["time_zone"]} \nLatitude: {parse_json["latitude"]} \nLongitude: {parse_json["longitude"]} \nMetro Code: {parse_json["metro_code"]} \n\nCreado por @lord_zeper')
+	bot.reply_to(message, f'----------------------------------------------- \n                    IP Detail\n----------------------------------------------- \nIP: {parse_json["ip"]} \nCountry Code: {parse_json["country_code"]} \nCountry Name: {parse_json["country_name"]} \nRegion Code: {parse_json["region_code"]} \nRegion Name: {parse_json["region_name"]} \nCity: {parse_json["city"]} \nZip Code: {parse_json["zip_code"]} \nTime Zone: {parse_json["time_zone"]} \nLatitude: {parse_json["latitude"]} \nLongitude: {parse_json["longitude"]} \nMetro Code: {parse_json["metro_code"]}')
 
 bot.polling()
